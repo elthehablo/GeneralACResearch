c1f2299740c71908340510d533d4edd767198794
Dan <83884799+elthehablo@users.noreply.github.com>
2024-02-02 17:12:45 +0100

    nerf suggestion

--- old_tuning_kara.sql
+++ old_tuning_kara.sql
@@ -1,98 +1,98 @@
 -- Servant's Quarters
-UPDATE `creature_template` SET `HealthModifier` = 3*21    WHERE `entry` = 16181; -- Rokad the Ravager
-UPDATE `creature_template` SET `HealthModifier` = 3*24.5  WHERE `entry` = 16180; -- Shadikith the Glider
-UPDATE `creature_template` SET `HealthModifier` = 3*24.5  WHERE `entry` = 16179; -- Hyakiss the Lurker
+UPDATE `creature_template` SET `HealthModifier` = 2.5*21    WHERE `entry` = 16181; -- Rokad the Ravager
+UPDATE `creature_template` SET `HealthModifier` = 2.5*24.5  WHERE `entry` = 16180; -- Shadikith the Glider
+UPDATE `creature_template` SET `HealthModifier` = 2.5*24.5  WHERE `entry` = 16179; -- Hyakiss the Lurker
 -- Attumen the Huntsman
-UPDATE `creature_template` SET `HealthModifier` = 3*35    WHERE `entry` = 16151; -- Midnight
-UPDATE `creature_template` SET `HealthModifier` = 3*35    WHERE `entry` = 16152; -- Attumen the Huntsman
+UPDATE `creature_template` SET `HealthModifier` = 2.5*35    WHERE `entry` = 16151; -- Midnight
+UPDATE `creature_template` SET `HealthModifier` = 2.5*35    WHERE `entry` = 16152; -- Attumen the Huntsman
 -- Moroes & Guests
-UPDATE `creature_template` SET `HealthModifier` = 3*35.7  WHERE `entry` = 15687; -- Moroes
-UPDATE `creature_template` SET `HealthModifier` = 3*8.4   WHERE `entry` = 19875; -- Baroness Dorothea Millstipe
-UPDATE `creature_template` SET `HealthModifier` = 3*8.4   WHERE `entry` = 19874; -- Baron Rafe Dreuger
-UPDATE `creature_template` SET `HealthModifier` = 3*8.4   WHERE `entry` = 19872; -- Lady Catriona Von'Indi
-UPDATE `creature_template` SET `HealthModifier` = 3*8.4   WHERE `entry` = 17007; -- Lady Keira Berrybuck
-UPDATE `creature_template` SET `HealthModifier` = 3*8.4   WHERE `entry` = 19876; -- Lord Robin Daris
-UPDATE `creature_template` SET `HealthModifier` = 3*8.4   WHERE `entry` = 19873; -- Lord Crispin Ference	
+UPDATE `creature_template` SET `HealthModifier` = 2.5*35.7  WHERE `entry` = 15687; -- Moroes
+UPDATE `creature_template` SET `HealthModifier` = 2.5*8.4   WHERE `entry` = 19875; -- Baroness Dorothea Millstipe
+UPDATE `creature_template` SET `HealthModifier` = 2.5*8.4   WHERE `entry` = 19874; -- Baron Rafe Dreuger
+UPDATE `creature_template` SET `HealthModifier` = 2.5*8.4   WHERE `entry` = 19872; -- Lady Catriona Von'Indi
+UPDATE `creature_template` SET `HealthModifier` = 2.5*8.4   WHERE `entry` = 17007; -- Lady Keira Berrybuck
+UPDATE `creature_template` SET `HealthModifier` = 2.5*8.4   WHERE `entry` = 19876; -- Lord Robin Daris
+UPDATE `creature_template` SET `HealthModifier` = 2.5*8.4   WHERE `entry` = 19873; -- Lord Crispin Ference	
 -- Maiden of Virtue
-UPDATE `creature_template` SET `HealthModifier` = 3*49    WHERE `entry` = 16457; -- Maiden of Virtue
+UPDATE `creature_template` SET `HealthModifier` = 2.5*49    WHERE `entry` = 16457; -- Maiden of Virtue
 -- Opera Event: Wizard of Oz
-UPDATE `creature_template` SET `HealthModifier` = 3*17.5  WHERE `entry` = 17535; -- Dorothee
-UPDATE `creature_template` SET `HealthModifier` = 3*3.5   WHERE `entry` = 17548; -- Tito
-UPDATE `creature_template` SET `HealthModifier` = 3*10.5  WHERE `entry` = 17543; -- Strawman
-UPDATE `creature_template` SET `HealthModifier` = 3*10.5  WHERE `entry` = 17547; -- Tinhead
-UPDATE `creature_template` SET `HealthModifier` = 3*10.5  WHERE `entry` = 17546; -- Roar
-UPDATE `creature_template` SET `HealthModifier` = 3*17.5  WHERE `entry` = 18168; -- The Crone
+UPDATE `creature_template` SET `HealthModifier` = 2.5*17.5  WHERE `entry` = 17535; -- Dorothee
+UPDATE `creature_template` SET `HealthModifier` = 2.5*3.5   WHERE `entry` = 17548; -- Tito
+UPDATE `creature_template` SET `HealthModifier` = 2.5*10.5  WHERE `entry` = 17543; -- Strawman
+UPDATE `creature_template` SET `HealthModifier` = 2.5*10.5  WHERE `entry` = 17547; -- Tinhead
+UPDATE `creature_template` SET `HealthModifier` = 2.5*10.5  WHERE `entry` = 17546; -- Roar
+UPDATE `creature_template` SET `HealthModifier` = 2.5*17.5  WHERE `entry` = 18168; -- The Crone
 -- Opera Event: Red Riding Hood
-UPDATE `creature_template` SET `HealthModifier` = 3*35    WHERE `entry` = 17521; -- The Big Bad Wolf
+UPDATE `creature_template` SET `HealthModifier` = 2.5*35    WHERE `entry` = 17521; -- The Big Bad Wolf
 -- Opera Event: Romulo and Julianne
-UPDATE `creature_template` SET `HealthModifier` = 3*17.5  WHERE `entry` = 17533; -- Romulo
-UPDATE `creature_template` SET `HealthModifier` = 3*17.5  WHERE `entry` = 17534; -- Julianne
+UPDATE `creature_template` SET `HealthModifier` = 2.5*17.5  WHERE `entry` = 17533; -- Romulo
+UPDATE `creature_template` SET `HealthModifier` = 2.5*17.5  WHERE `entry` = 17534; -- Julianne
 -- The Curator
-UPDATE `creature_template` SET `HealthModifier` = 3*80.5  WHERE `entry` = 15691; -- The Curator
+UPDATE `creature_template` SET `HealthModifier` = 2.5*80.5  WHERE `entry` = 15691; -- The Curator
 -- Terestian Illhoof
-UPDATE `creature_template` SET `HealthModifier` = 3*70    WHERE `entry` = 15688; -- Terestian Illhoof
-UPDATE `creature_template` SET `HealthModifier` = 3*8.4   WHERE `entry` = 17229; -- Kil'rek
-UPDATE `creature_template` SET `HealthModifier` = 3*0.63  WHERE `entry` = 17267; -- Fiendish Imp
+UPDATE `creature_template` SET `HealthModifier` = 2.5*70    WHERE `entry` = 15688; -- Terestian Illhoof
+UPDATE `creature_template` SET `HealthModifier` = 2.5*8.4   WHERE `entry` = 17229; -- Kil'rek
+UPDATE `creature_template` SET `HealthModifier` = 2.5*0.63  WHERE `entry` = 17267; -- Fiendish Imp
 -- Shade of Aran
-UPDATE `creature_template` SET `HealthModifier` = 3*112, `ManaModifier` = 0.5*22   WHERE `entry` = 16524; -- Shade of Aran
-UPDATE `creature_template` SET `HealthModifier` = 3*1.4   WHERE `entry` = 17167; -- Conjured Elemental
+UPDATE `creature_template` SET `HealthModifier` = 2.5*112, `ManaModifier` = 0.5*22   WHERE `entry` = 16524; -- Shade of Aran
+UPDATE `creature_template` SET `HealthModifier` = 2.5*1.4   WHERE `entry` = 17167; -- Conjured Elemental
 -- Nightbane
-UPDATE `creature_template` SET `HealthModifier` = 3*122.5 WHERE `entry` = 17225; -- Nightbane
-UPDATE `creature_template` SET `HealthModifier` = 3*1.75  WHERE `entry` = 17261; -- Restless Skeleton
+UPDATE `creature_template` SET `HealthModifier` = 2*122.5 WHERE `entry` = 17225; -- Nightbane
+UPDATE `creature_template` SET `HealthModifier` = 2.5*1.75  WHERE `entry` = 17261; -- Restless Skeleton
 -- Netherspite
-UPDATE `creature_template` SET `HealthModifier` = 3*140   WHERE `entry` = 15689; -- Netherspite
+UPDATE `creature_template` SET `HealthModifier` = 2.5*140   WHERE `entry` = 15689; -- Netherspite
 -- Prince Malchezaar
-UPDATE `creature_template` SET `HealthModifier` = 3*105   WHERE `entry` = 15690; -- Prince Malchezaar
+UPDATE `creature_template` SET `HealthModifier` = 2.5*105   WHERE `entry` = 15690; -- Prince Malchezaar
 -- Trash
-UPDATE `creature_template` SET `HealthModifier` = 3*1.4  WHERE `entry` = 16409; -- Phantom Guest 
-UPDATE `creature_template` SET `HealthModifier` = 3*1.4  WHERE `entry` = 16468; -- Spectral Patron 
-UPDATE `creature_template` SET `HealthModifier` = 3*1.4  WHERE `entry` = 16492; -- Syphoner 
-UPDATE `creature_template` SET `HealthModifier` = 3*1.4  WHERE `entry` = 16491; -- Mana Feeder 
-UPDATE `creature_template` SET `HealthModifier` = 3*2.1  WHERE `entry` = 16170; -- Coldmist Stalker 
-UPDATE `creature_template` SET `HealthModifier` = 3*2.1  WHERE `entry` = 16173; -- Shadowbat 
-UPDATE `creature_template` SET `HealthModifier` = 3*2.1  WHERE `entry` = 16539; -- Homunculus 
-UPDATE `creature_template` SET `HealthModifier` = 3*2.45 WHERE `entry` = 16178; -- Phase Hound 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16389; -- Spectral Apprentice 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16406; -- Phantom Attendant 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16407; -- Spectral Servant 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16411; -- Spectral Chef 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16412; -- Ghostly Baker 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16415; -- Skeletal Waiter 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16424; -- Spectral Sentry 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16425; -- Phantom Guardsman 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16175; -- Vampiric Shadowbat 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16176; -- Shadowbeast 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16525; -- Spell Shade 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16530; -- Mana Warp 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 16540; -- Shadow Pillager 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.2  WHERE `entry` = 15551; -- Spectral Stable Hand 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.9  WHERE `entry` = 16171; -- Coldmist Widow 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*4.9  WHERE `entry` = 16177; -- Dreadbeast 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16410; -- Spectral Retainer 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16414; -- Ghostly Steward 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16174; -- Greater Shadowbat 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16459; -- Wanton Hostess 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16460; -- Night Mistress 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16461; -- Concubine 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16470; -- Ghostly Philanthropist
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16473; -- Spectral Performer 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16489; -- Chaotic Sentience 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16526; -- Sorcerous Shade 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16529; -- Magical Horror 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*5.6  WHERE `entry` = 16544; -- Ethereal Thief 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*6.3  WHERE `entry` = 16408; -- Phantom Valet 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*6.3  WHERE `entry` = 16472; -- Phantom Stagehand 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*6.3  WHERE `entry` = 15547; -- Spectral Charger 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*6.3  WHERE `entry` = 15548; -- Spectral Stallion 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*7    WHERE `entry` = 16481; -- Ghastly Haunt 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*7    WHERE `entry` = 16482; -- Trapped Soul 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*7    WHERE `entry` = 16545; -- Ethereal Spellfilcher 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*7    WHERE `entry` = 16595; -- Fleshbeast 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*8.4  WHERE `entry` = 16471; -- Skeletal Usher 
-UPDATE `creature_template` SET `HealthModifier` = 2.5*8.4  WHERE `entry` = 16485; -- Arcane Watchman 
-UPDATE `creature_template` SET `HealthModifier` = 2*11.2 WHERE `entry` = 16504; -- Arcane Protector 
-UPDATE `creature_template` SET `HealthModifier` = 2*11.2 WHERE `entry` = 16596; -- Greater Fleshbeast 
+UPDATE `creature_template` SET `HealthModifier` = 2.5*1.4  WHERE `entry` = 16409; -- Phantom Guest 
+UPDATE `creature_template` SET `HealthModifier` = 2.5*1.4  WHERE `entry` = 16468; -- Spectral Patron 
+UPDATE `creature_template` SET `HealthModifier` = 2.5*1.4  WHERE `entry` = 16492; -- Syphoner 
+UPDATE `creature_template` SET `HealthModifier` = 2.5*1.4  WHERE `entry` = 16491; -- Mana Feeder 
+UPDATE `creature_template` SET `HealthModifier` = 2.5*2.1  WHERE `entry` = 16170; -- Coldmist Stalker 
+UPDATE `creature_template` SET `HealthModifier` = 2.5*2.1  WHERE `entry` = 16173; -- Shadowbat 
+UPDATE `creature_template` SET `HealthModifier` = 2.5*2.1  WHERE `entry` = 16539; -- Homunculus 
+UPDATE `creature_template` SET `HealthModifier` = 2.5*2.45 WHERE `entry` = 16178; -- Phase Hound 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16389; -- Spectral Apprentice 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16406; -- Phantom Attendant 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16407; -- Spectral Servant 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16411; -- Spectral Chef 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16412; -- Ghostly Baker 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16415; -- Skeletal Waiter 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16424; -- Spectral Sentry 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16425; -- Phantom Guardsman 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16175; -- Vampiric Shadowbat 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16176; -- Shadowbeast 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16525; -- Spell Shade 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16530; -- Mana Warp 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 16540; -- Shadow Pillager 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.2  WHERE `entry` = 15551; -- Spectral Stable Hand 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.9  WHERE `entry` = 16171; -- Coldmist Widow 
+UPDATE `creature_template` SET `HealthModifier` = 2*4.9  WHERE `entry` = 16177; -- Dreadbeast 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16410; -- Spectral Retainer 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16414; -- Ghostly Steward 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16174; -- Greater Shadowbat 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16459; -- Wanton Hostess 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16460; -- Night Mistress 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16461; -- Concubine 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16470; -- Ghostly Philanthropist
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16473; -- Spectral Performer 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16489; -- Chaotic Sentience 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16526; -- Sorcerous Shade 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16529; -- Magical Horror 
+UPDATE `creature_template` SET `HealthModifier` = 2*5.6  WHERE `entry` = 16544; -- Ethereal Thief 
+UPDATE `creature_template` SET `HealthModifier` = 2*6.3  WHERE `entry` = 16408; -- Phantom Valet 
+UPDATE `creature_template` SET `HealthModifier` = 2*6.3  WHERE `entry` = 16472; -- Phantom Stagehand 
+UPDATE `creature_template` SET `HealthModifier` = 2*6.3  WHERE `entry` = 15547; -- Spectral Charger 
+UPDATE `creature_template` SET `HealthModifier` = 2*6.3  WHERE `entry` = 15548; -- Spectral Stallion 
+UPDATE `creature_template` SET `HealthModifier` = 2*7    WHERE `entry` = 16481; -- Ghastly Haunt 
+UPDATE `creature_template` SET `HealthModifier` = 2*7    WHERE `entry` = 16482; -- Trapped Soul 
+UPDATE `creature_template` SET `HealthModifier` = 2*7    WHERE `entry` = 16545; -- Ethereal Spellfilcher 
+UPDATE `creature_template` SET `HealthModifier` = 2*7    WHERE `entry` = 16595; -- Fleshbeast 
+UPDATE `creature_template` SET `HealthModifier` = 2*8.4  WHERE `entry` = 16471; -- Skeletal Usher 
+UPDATE `creature_template` SET `HealthModifier` = 2*8.4  WHERE `entry` = 16485; -- Arcane Watchman 
+UPDATE `creature_template` SET `HealthModifier` = 1.5*11.2 WHERE `entry` = 16504; -- Arcane Protector 
+UPDATE `creature_template` SET `HealthModifier` = 1.5*11.2 WHERE `entry` = 16596; -- Greater Fleshbeast 
 
 DELETE FROM `zone_difficulty_info` WHERE `MapID`=532;
 INSERT INTO `zone_difficulty_info` (`MapID`, `PhaseMask`, `HealingNerfValue`, `AbsorbNerfValue`, `MeleeDmgBuffValue`, `SpellDmgBuffValue`, `Enabled`, `Comment`) VALUES
-(532, 0, 0.75, 0.75, 1.25, 1.25, 1, 'Karazhan');
+(532, 0, 0.75, 0.75, 1.15, 1.15, 1, 'Karazhan');
